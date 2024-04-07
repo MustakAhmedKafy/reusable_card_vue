2456581eacf5ca5d4c86495da886ea8950f1cc25
Mustak Ahmed Kafy <43313297+MustakAhmedKafy@users.noreply.github.com>
2024-04-07 17:46:39 +0600

    card data add using props

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import Card from "./components/Card.vue";
+
+const data = [
+  {
+    image: "img/product-1.jpg",
+    location: "California",
+    price: 87,
+    rating: 4,
+    time: 15,
+    title: "Photo by drew",
+  },
+  {
+    image: "img/product-2.jpg",
+    location: "Los Angels",
+    price: 90,
+    rating: 5,
+    time: 17,
+    title: "Photo by Mustak",
+  },
+];
+</script>
+
+<template>
+  <div>
+    <!-- <Card
+      v-for="item in data"
+      :key="item.title"
+      :image="item.image"
+      :location="item.location"
+      :price="item.price"
+      :rating="item.rating"
+      :time="item.time"
+      :title="item.title"
+    /> -->
+    <div class="container mx-auto">
+      <div class="md:flex md:flex-wrap">
+        <div class="md:w-1/2 px-4 mb-8">
+          <Card v-for="item in data" :key="item.title" v-bind="item" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
